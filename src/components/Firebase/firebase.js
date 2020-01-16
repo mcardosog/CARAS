98b7e503d4578faf3037cf24b69529a53a90636e
Marco Cardoso <marcocardoso@Marcos-MacBook-Pro.local>
2020-01-16 10:25:58 -0500

    Password forget created

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -15,11 +15,21 @@ const firebaseConfig = {
 
   class Firebase {
     constructor() {
-      app.initializeApp(config);
+      app.initializeApp(firebaseConfig);
       this.auth = app.auth();
     }
 
     doCreateUserWithEmailAndPassword = (email, password) =>
     this.auth.createUserWithEmailAndPassword(email, password);
+
+    doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+    
+    doPasswordUpdate = password =>
+        this.auth.currentUser.updatePassword(password);
   }
   export default Firebase;
