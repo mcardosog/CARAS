47f0dd721a4429aba094a2fb36a7ebdbe883a6ab
Marco Cardoso <marcocardoso@Marcos-MacBook-Pro.local>
2020-01-16 10:36:30 -0500

    Seting up realtime DB

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,12 +1,17 @@
-import React, { Component } from 'react';
-
-class Account extends Component {
-  render() {
-    return (
+import React from 'react';
+import { AuthUserContext, withAuthorization } from '../Session';
+import { PasswordForgetForm } from '../PasswordForget';
+import PasswordChangeForm from '../PasswordChange';
+const AccountPage = () => (
+  <AuthUserContext.Consumer>
+    {authUser => (
       <div>
-        ACCOUNT
+        <h1>Account: {authUser.email}</h1>
+        <PasswordForgetForm />
+        <PasswordChangeForm />
       </div>
-    );
-  }
-}
-export default Account;
+    )}
+  </AuthUserContext.Consumer>
+);
+const condition = authUser => !!authUser;
+export default withAuthorization(condition)(AccountPage);
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,10 @@
-import React, { Component } from 'react';
-
-class Home extends Component {
-  render() {
-    return (
-      <div>
-        HOME
-      </div>
-    );
-  }
-}
-export default Home;
+import React from 'react';
+import { withAuthorization } from '../Session';
+const HomePage = () => (
+  <div>
+    <h1>Home Page</h1>
+    <p>The Home Page is accessible by every signed in user.</p>
+  </div>
+);
+const condition = authUser => !!authUser;
+export default withAuthorization(condition)(HomePage);
