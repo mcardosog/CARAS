3a2bf509d97cf6a9de7a53a0f7cdc4f5d6e06758
Marco <cardosogutierrez.marco@gmail.com>
2020-03-27 15:34:13 -0400

    Fixed callback functions when the information is requested from FireBase.

--- src/components/FaceRecognition/index.js
+++ src/components/FaceRecognition/index.js
@@ -17,69 +17,15 @@ class Face_Recognition extends Component {
     render() {
         const organization = 'org1';
         const fb = this.props.firebase;
-        const db = { 1:'Marco', 2:'Captain America', 3:'Captain Marvel', 4:'Hawkeye',                  //<----- DEBUG ONLY
-                    5:'Jim Rhodes', 6:'Thor', 7:'Tony Stark', 8:'Adriana'};
-
         /**
          * @param userID
-         * @returns {Promise<string[]>} set with user stored images
+         * @returns {LabeledFaceDescriptors} set with user stored images
          */
-        async function loadImageSet(userID) {
 
-            /*
-            if (userID <= 0 || userID >= db.length) {
-                return null;
-            }
-             */
-
-            let descriptionSet = await fb.getDescriptors(organization,'user1');
-            var descriptions = [];
-
-            for(var i=0; i<descriptionSet.length;i++) {
-                let d = descriptionSet[i];
-                descriptions.push(Float32Array.from((d.value.value)));
-            }
-            if(descriptions.length == 0) { return null; }
-
-            return new faceapi.LabeledFaceDescriptors('user1', descriptions);
-            /*
-            const path = '/labeled_images';
-            const labels = [db[userID]];
-            console.log(labels);
-            return Promise.all(
-                labels.map(async label => {
-                    const descriptions = []
-                    var end = 2;
-                    for (var i = 0; i <= end; i++) {
-                        const img = await faceapi.fetchImage(`${path}/${label}/${i}.jpg`)
-                        const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
-                        descriptions.push(detections.descriptor)
-
-                    }
-                    console.log(descriptions);
-                    return new faceapi.LabeledFaceDescriptors(label, descriptions)
-                })
-            )
-             */
-
-            /*
-            const path = '/labeled_images';
-            const labels = [db[userID]];
-            console.log(labels);
-            return Promise.all(
-                labels.map(async label => {
-                    const descriptions = []
-                    var end = 2;
-                    for (var i = 0; i <= end; i++) {
-                        const img = await faceapi.fetchImage(`${path}/${label}/${i}.jpg`)
-                        const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
-                        descriptions.push(detections.descriptor)
-                        console.log(descriptions);
-                    }
-                    return new faceapi.LabeledFaceDescriptors(label, descriptions)
-                })
-            )
-            */
+        async function loadUserDescriptor(userID) {
+            let descriptionSet = await fb.getDescriptors(organization,userID);
+            if(descriptionSet.length == 0) { return null; }
+            return new faceapi.LabeledFaceDescriptors(userID, descriptionSet);
         }
 
         /**
@@ -89,47 +35,39 @@ class Face_Recognition extends Component {
          * Function store the image a compare it with the corresponding userID to verify if it match
          */
         async function handleTakePhoto (dataUri) {
+
             const userID = document.getElementById("userId").value;
             if(userID == '') {
                 alert("Please enter an user id");
                 return;
             }
 
-            const imageSet = await loadImageSet(userID);
-            if(imageSet == null) {
+            const descriptorSet =  await (loadUserDescriptor(userID));
+            if(descriptorSet == null || descriptorSet.length == 0) {
                 alert("Invalid user id");
                 return;
             }
-            console.log('HERE');
-            let blob = await fetch(dataUri).then(r => r.blob());    //Build Image
-            const image = await faceapi.bufferToImage(blob);
-            const detection = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors();
+            let blob =  await fetch(dataUri).then(r => r.blob());    //Build Image
+            const image =  await faceapi.bufferToImage(blob);
+            const detection =  await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors();
             if(detection.length === 0) {
                 alert("No face detected. Please try again.");
                 return;
             }
 
-            //console.log('DETECTION\n')
-            //console.log(detection[0].descriptor);
-
-            //fb.insertDescriptor('org1','user1',detection[0].descriptor);
-
-
-            const faceMatcher = new faceapi.FaceMatcher(imageSet, 0.6);
+            const faceMatcher = await new faceapi.FaceMatcher(descriptorSet, 0.6);
             const displaySize = { width: image.width, height: image.height };
-            const resizedDetections = faceapi.resizeResults(detection, displaySize);
+            const resizedDetections = await faceapi.resizeResults(detection, displaySize);
             const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor));
-            //console.log(results);                                                         //<----- DEBUG ONLY
             results.forEach((result, i) => {
                 console.log( 'Subject '+i+' '+((1 - result.distance)*100)+'%');
             })
 
-            const detectionsWithAgeAndGender = await faceapi.detectAllFaces(image).withAgeAndGender()
+            const detectionsWithAgeAndGender =  await faceapi.detectAllFaces(image).withAgeAndGender()
             console.log(detectionsWithAgeAndGender)
             //Yes / No
 
             //fb.uploadImage('test','1',blob);
-
         }
 
         /**
@@ -157,7 +95,9 @@ class Face_Recognition extends Component {
                     </div>
                     <div>
                         <h2> INFORMATION </h2>
-
+                        <p>Accuracy</p>
+                        <p>Age</p>
+                        <p>Sex</p>
                     </div>
                 </div>
             </div>
--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -15,201 +15,163 @@ const config = {
     measurementId: "G-FYCFPK2QDN"
   };
 
-  class Firebase{
-    //Comment
-    constructor() {
-        app.initializeApp(config);
-        this.auth = app.auth();
-        this.db = app.database();
-    }
+  class Firebase {
+      //Comment
+      constructor() {
+          app.initializeApp(config);
+          this.auth = app.auth();
+          this.db = app.database();
+      }
 
-    //Auth
+      //Auth
 
-    doCreateUserWithEmailAndPassword = (email, password) =>
-        this.auth.createUserWithEmailAndPassword(email, password);
+      doCreateUserWithEmailAndPassword = (email, password) =>
+          this.auth.createUserWithEmailAndPassword(email, password);
 
-    doSignInWithEmailAndPassword = (email, password) =>
-        this.auth.signInWithEmailAndPassword(email, password);
+      doSignInWithEmailAndPassword = (email, password) =>
+          this.auth.signInWithEmailAndPassword(email, password);
 
-    doSignOut = () => this.auth.signOut();
+      doSignOut = () => this.auth.signOut();
 
-    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+      doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
 
-    doPasswordUpdate = password =>
-        this.auth.currentUser.updatePassword(password);
+      doPasswordUpdate = password =>
+          this.auth.currentUser.updatePassword(password);
 
-    doDeleteUser = () => {
-        var user = this.auth.currentUser;
-        console.log(user);
-        this.auth.signOut();
+      doDeleteUser = () => {
+          var user = this.auth.currentUser;
+          console.log(user);
+          this.auth.signOut();
 
-        user.delete().then(function() {
-            console.log("User deleted.");
-        }).catch(function(error) {
-            console.log("Error deleting user.")
-        });
-    };
+          user.delete().then(function () {
+              console.log("User deleted.");
+          }).catch(function (error) {
+              console.log("Error deleting user.")
+          });
+      };
 
-    //Data
-      getElementsInPath = (path) => {
+      //Data
+      getElementsInPath = async (path) => {
           var data = [];
           var ref = this.db.ref(path);
-          ref.on('value',function (snapshot) {
-              snapshot.forEach(function (childSnapshot){
-                  data.push({
-                      uid:childSnapshot.key,
-                      value:childSnapshot.val()
-                  });
+          var snapshot = await ref.once('value');
+          snapshot.forEach(function (childSnapshot) {
+              data.push({
+                  uid: childSnapshot.key,
+                  value: childSnapshot.val()
               });
           });
           return data;
       }
 
-    deleteElement = (path, elementID) => {
-        try {
-            this.db.ref(path).child(elementID).remove();
-        } catch (error) {
-            console.log(error);
-        }
-    }
+      deleteElement = (path, elementID) => {
+          try {
+              this.db.ref(path).child(elementID).remove();
+          } catch (error) {
+              console.log(error);
+          }
+      }
 
-    test() {
-        console.log("YESSSSSS!!!!!!");
-    }
+      getImages(organization, userID) {
 
-    getImages(organization, userID) {
+          var data = [];
+          var listRef = app.storage().ref().child('images/' + organization + '/' + userID + '/');
 
-        var data = [];
-        var listRef = app.storage().ref().child('images/'+organization+'/'+userID+'/');
+          // Find all the prefixes and items.
+          listRef.listAll().then(function (res) {
+              res.items.forEach(function (itemRef) {
+                  // All the items under listRef.
+                  data.push(itemRef.getDownloadURL());
 
-        // Find all the prefixes and items.
-        listRef.listAll().then(function(res) {
-            res.items.forEach(function(itemRef) {
-                // All the items under listRef.
-                data.push(itemRef.getDownloadURL());
+              });
+          }).catch(function (error) {
+              console.log("Unable to load image set");
+          });
+          return data;
+      }
 
-            });
-        }).catch(function(error) {
-            console.log("Unable to load image set");
-        });
-        return data;
-    }
+      uploadImage(organization, userID, imageBlob) {
+          var storageRef = app.storage().ref();
+          // File or Blob named mountains.jpg
+          var file = imageBlob;
+          var metadata = {
+              contentType: 'image/jpeg'
+          };
+          // Upload file and metadata to the object 'images/mountains.jpg'
+          let fileName = Date.now() + '.jpeg';
+          var uploadTask = storageRef.child('images/' + organization + '/' + userID + '/' + fileName).put(file, metadata);
 
-    uploadImage(organization, userID, imageBlob) {
-        var storageRef = app.storage().ref();
-        // File or Blob named mountains.jpg
-        var file = imageBlob;
-        var metadata = {
-            contentType: 'image/jpeg'
-        };
-        // Upload file and metadata to the object 'images/mountains.jpg'
-        let fileName = Date.now()+'.jpeg';
-        var uploadTask = storageRef.child('images/'+organization+'/'+userID+'/'+fileName).put(file, metadata);
+          // Listen for state changes, errors, and completion of the upload.
+          uploadTask.on(app.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+              function (snapshot) {
+                  // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                  var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                  console.log('Upload is ' + progress + '% done');
+                  switch (snapshot.state) {
+                      case app.storage.TaskState.PAUSED: // or 'paused'
+                          console.log('Upload is paused');
+                          break;
+                      case app.storage.TaskState.RUNNING: // or 'running'
+                          console.log('Upload is running');
+                          break;
+                  }
+              }, function (error) {
+                  // https://firebase.google.com/docs/storage/web/handle-errors
+                  switch (error.code) {
+                      case 'storage/unauthorized':
+                          console.log("Error uploading: unauthorized ");
+                          break;
+                      case 'storage/canceled':
+                          console.log("Error uploading: canceled ");
+                          break;
+                      case 'storage/unknown':
+                          console.log("Error uploading: unknown");
+                          break;
+                  }
+              }, function () {
+                  uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+                      console.log('File available at', downloadURL);
+                  });
+              });
+      }
 
-        // Listen for state changes, errors, and completion of the upload.
-        uploadTask.on(app.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-            function(snapshot) {
-                // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-                var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                console.log('Upload is ' + progress + '% done');
-                switch (snapshot.state) {
-                    case app.storage.TaskState.PAUSED: // or 'paused'
-                        console.log('Upload is paused');
-                        break;
-                    case app.storage.TaskState.RUNNING: // or 'running'
-                        console.log('Upload is running');
-                        break;
-                }
-            }, function(error) {
-                // https://firebase.google.com/docs/storage/web/handle-errors
-                switch (error.code) {
-                    case 'storage/unauthorized':
-                        console.log("Error uploading: unauthorized ");
-                        break;
-                    case 'storage/canceled':
-                        console.log("Error uploading: canceled ");
-                        break;
-                    case 'storage/unknown':
-                        console.log("Error uploading: unknown");
-                        break;
-                }
-            }, function() {
-                uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-                    console.log('File available at', downloadURL);
-                });
-            });
-    }
-
-    insertDescriptor = (organization, userID, descriptor) => {
-          this.db.ref('organizations/'+organization+'/'+userID+'/descriptors/').push({
+      insertDescriptor = (organization, userID, descriptor) => {
+          this.db.ref('organizations/' + organization + '/' + userID + '/descriptors/').push({
               date: Date.now(),
               value: descriptor,
           });
 
       };
 
-     getDescriptors = (organization, userID) => {
-         const path = 'organizations/'+organization+'/'+userID+'/descriptors/';
-         return this.getElementsInPath(path);
+      getDescriptors = async (organization, userID) => {
+          const path = 'organizations/' + organization + '/' + userID + '/descriptors/';
+          const tempDescriptors = await this.getElementsInPath(path);
+          const descriptors = [];
+          for (var i = 0; i < tempDescriptors.length; i++) {
+              let d = tempDescriptors[i];
+              descriptors.push(Float32Array.from((d.value.value)));
+          }
+          return descriptors;
+      };
 
-     };
-    insertRequest = (description, file, name, userID) => {
-        this.db.ref('requests').push({
-            description: description,
-            file: file,
-            name: name,
-            userID: userID
-        });
-    }
+      getElementsByUserID = (path, userID) => {
+          var data = [];
+          var ref = this.db.ref(path);
+          ref.on('value', function (snapshot) {
+              snapshot.forEach(function (childSnapshot) {
+                  if (childSnapshot.val().userID === userID) {
+                      data.push({
+                          uid: childSnapshot.key,
+                          value: childSnapshot.val()
+                      });
+                  }
+              });
+          });
+          return data;
+      }
 
-    deleteRequest = (requestID) => {
-        this.deleteElement('requests', requestID);
-    }
-
-    deleteFile = (fileID) => {
-        this.deleteElement('files', fileID);
-    }
-
-    insertNewsLetters = (content, date, linked_video, title) => {
-        this.db.ref('newsletters').push({
-            content: content,
-            date: date,
-            linked_video: linked_video,
-            title: title
-        });
-    }
-
-    deleteNewsLetters = (title, date) => {
-        var newsletterID = '';
-        var ref = this.db.ref('newsletters');
-        ref.on('value', function (snapshot){
-            snapshot.forEach(function (childSnapshot){
-                if(childSnapshot.val().title === title && childSnapshot.val().date === date){
-                    newsletterID = childSnapshot.key
-                }
-            });
-        });
-        this.deleteElement('newsletters', newsletterID)
-    }
-
-    getElementsByUserID = (path, userID) => {
-        var data = [];
-        var ref = this.db.ref(path);
-        ref.on('value',function (snapshot) {
-            snapshot.forEach(function (childSnapshot){
-                if(childSnapshot.val().userID === userID) {
-                    data.push({
-                        uid:childSnapshot.key,
-                        value:childSnapshot.val()
-                    });
-                }
-            });
-        });
-        return data;
-    }
-
-    // *** User API ***
-    user = uid => this.db.ref(`users/${uid}`);
-    users = () => this.db.ref('users');
-}
+      // *** User API ***
+      user = uid => this.db.ref(`users/${uid}`);
+      users = () => this.db.ref('users');
+  }
   export default Firebase;
