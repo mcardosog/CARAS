0fc576e69334490e8f724ac5c3b36ba425fb5571
Alejandro Alonso <21010777+Alexalonso04@users.noreply.github.com>
2020-03-27 11:11:44 -0400

    created an event panel to display active events

---
+++ src/UIComponents/eventPanel.js
@@ -0,0 +1,38 @@
+﻿import React from "react";
+import { Grid, Container, Button, Divider, Header } from "semantic-ui-react";
+
+import EventCard from "./EventCard";
+
+export default function EventPanel({ events }) {
+    console.log(events);
+    return (
+        <>
+            <Grid>
+                <Grid.Row>
+                    <Grid.Column>
+                        <Button
+                            content="Add Event"
+                            icon="add"
+                            labelPosition="left"
+                            floated="right"
+                        />
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+
+            <Divider horizontal>
+                <Header as="h2">Active Events</Header>
+            </Divider>
+            <Container>
+                <Grid stackable columns={3}>
+                    <Grid.Row verticalAlign="middle">
+                        {events &&
+                            events.map((event, index) => (
+                                <EventCard event={event} key={index} />
+                            ))}
+                    </Grid.Row>
+                </Grid>
+            </Container>
+        </>
+    );
+}
