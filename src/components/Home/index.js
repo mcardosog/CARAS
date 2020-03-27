499ed6b8b55f5df19436e0607b6a7781e4b392dc
Alejandro Alonso <21010777+Alexalonso04@users.noreply.github.com>
2020-03-27 11:18:27 -0400

    added the event panel in the home page for testing purposes

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,10 +2,36 @@ import React from 'react';
 import { withAuthorization } from '../Session';
 import { Link } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
+
+import EventPanel from '../../UIComponents/EventPanel';
+const events = [
+    {
+        name: "eventA",
+        date: "July, 2020",
+        description: "some event i made",
+        attendance: 24,
+        totalCount: 43
+    },
+    {
+        name: "eventB",
+        date: "July, 2058",
+        description: "another event i made",
+        attendance: 24,
+        totalCount: 43
+    },
+    {
+        name: "eventC",
+        date: "July, 3000",
+        description: "y otro mas event i made",
+        attendance: 24,
+        totalCount: 43
+    }
+];
+
 const Home = () => (
-    <div>
-        HOME
-    </div>
+    <>
+        <EventPanel events={events}/>
+    </>
 );
 const condition = authUser => !!authUser;
 export default withAuthorization(condition)(Home);
