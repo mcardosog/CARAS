51d8c5009610635ebfb7f433302f18767a00355e
Alejandro Alonso <21010777+Alexalonso04@users.noreply.github.com>
2020-04-02 23:53:46 -0400

    event code now only takes numerical inputs

--- src/UIComponents/CreateEventForm.js
+++ src/UIComponents/CreateEventForm.js
@@ -5,6 +5,8 @@ import { Form, Grid, Button, Icon } from "semantic-ui-react";
 import SemanticDatepicker from 'react-semantic-ui-datepickers';
 
 export default function EventForm({organization, addEvent, updateEvents, closeModal}) {
+    const [code, setCode] = useState('');
+    
     const [values, setValues] = useState({
         id: "",
         name: "",
@@ -103,10 +105,15 @@ export default function EventForm({organization, addEvent, updateEvents, closeMo
                                 label="Event Code"
                                 name="code"
                                 type="text"
-                                // value={values.code}
+                                value={code}
                                 maxLength="5"
                                 onChange={(param, data) => {
-                                    onChange(data.name, data.value.toUpperCase());
+                                    //only allow numeric values to be inputted
+                                    var regex = (/[0-9]|\./);
+                                    if (regex.test(data.value)) {
+                                        setCode(data.value);
+                                        onChange(data.name, data.value);
+                                    }
                                 }}
                             />
                             <Form.Input
