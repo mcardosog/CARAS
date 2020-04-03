e4526652e4c703971129b170db4f62da908a853a
Alejandro Alonso <21010777+Alexalonso04@users.noreply.github.com>
2020-04-03 14:29:53 -0400

    added user panel table

--- src/components/UserPanel/index.js
+++ src/components/UserPanel/index.js
@@ -4,41 +4,109 @@ import { withFirebase } from '../Firebase';
 import { Link } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
 
-import EventPanel from '../../UIComponents/EventPanel'
+import { 
+    Table, 
+    Grid, 
+    Container,
+    Header, 
+    Dimmer, 
+    Divider, 
+    Icon, 
+    Loader, 
+    Segment, 
+    Button} from 'semantic-ui-react';
 
 class UserPanel extends Component {
     constructor(props) {
         super(props);
         this.state = {
             organization: '',
-            addEvent: null,
-            events: [],
-            updateEvents: null,
+            users: [],
+            addUser: null,
+            deleteUser: null,
+            updateUsers: null
         }
     }
 
     async componentDidMount() {
         const organization = await this.props.firebase.getOrganization();
-        const events = await this.props.firebase.getEventsPreview(organization);
-        const addEvent = this.props.firebase.addEvent;
-        const updateEvents  = async () => {
-            const events =  await this.props.firebase.getEventsPreview(this.state.organization);
-            this.setState({ events: events });
+        const users = await this.props.firebase.getUsersPreview(organization);
+        const addUser = this.props.firebase.addUser;
+        const deleteUser = this.props.firebase.deleteUser;
+        const updateUsers  = async () => {
+            const users =  await this.props.firebase.getUsersPreview(this.state.organization);
+            this.setState({ users: users });
         }
         this.setState({
             organization: organization,
-            events: events,
-            addEvent : addEvent,
-            updateEvents: updateEvents,
+            users: users,
+            addUser : addUser,
+            deleteUser: deleteUser,
+            updateUsers: updateUsers
         });
     }
 
     render() {
-        const {organization, events, addEvent, updateEvents} = this.state;
+        const {organization, users, addUser, deleteUser, updateUsers} = this.state;
+        
+        
         return (
-            <div>
-                USER PANEL
-            </div>
+            <>
+            <Container>
+                <Grid
+                columns={1}>
+                    <Grid.Row>
+                        <Header as='h1'>User Panel</Header>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Button
+                                content="Add User"
+                                icon="add"
+                                labelPosition="left"
+                                floated="right"
+                            />
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Container>
+
+            <Divider horizontal>
+                <Header as="h2">{organization} Users </Header>
+            </Divider>
+            <Dimmer active={users ? false : true} inverted>
+                <Loader/>
+             </Dimmer>
+            {users === undefined || users.length === 0 
+            ? (
+              <Segment placeholder>
+                <Header icon>
+                  <Icon name='inbox' size='large'/>
+                  <p>There are currently no users registered</p>
+                </Header>
+              </Segment>
+                )
+            :(
+                <Container>
+                    <Grid>
+                        <Grid.Row>
+                            <Table unstackable>
+                                <Table.Header>
+                                    <Table.Row textAlign='center'>
+                                        <Table.HeaderCell>First Name </Table.HeaderCell>
+                                        <Table.HeaderCell>Last Name  </Table.HeaderCell>
+                                        <Table.HeaderCell>Level      </Table.HeaderCell>
+                                    </Table.Row>
+                                </Table.Header>
+                                <Table.Body>
+                                </Table.Body>
+                             </Table>
+                        </Grid.Row>
+                    </Grid>
+                </Container>
+                )
+            }
+            </>
         )
     }
 
