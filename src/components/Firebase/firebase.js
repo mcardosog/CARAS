fdc87dbd5b2d9acd772b92ec72b6bf8555f4c8e9
Melissa <mperez7009@ufl.edu>
2020-04-09 15:30:06 -0400

    updating user preview

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -243,14 +243,19 @@ const config = {
                   'lastName':'',
                   'level':'',
                   'email':'',
-                  'userID':''
+                  'userID':'',
+                  'age':'',
+                  'sex':''
               };
               userInformation.firstName = tempElement[i].value.firstName;
               userInformation.lastName = tempElement[i].value.lastName;
               userInformation.level = tempElement[i].value.level;
               userInformation.email = tempElement[i].value.email;
               userInformation.userID = tempElement[i].uid;
+              userInformation.age = tempElement[i].value.age;
+              userInformation.sex = tempElement[i].value.sex;
               userResults.push(userInformation);
+              console.log(userInformation);
           }
           return userResults;
       }
